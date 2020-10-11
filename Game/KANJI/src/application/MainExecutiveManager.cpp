234c61644ef44ba9bd77a2a179f232718b83d7ff
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-10-12 00:02:01 +0900

    FontAsset登録情報をmainへ移動

--- Game/KANJI/src/application/MainExecutiveManager.cpp
+++ Game/KANJI/src/application/MainExecutiveManager.cpp
@@ -9,8 +9,16 @@ dx::app::ExecutiveManager::Desc createExecutiveDesc() {
     .windowTitle = U"クレイジーサイコレズ",
     .windowSize = s3d::Size(1280, 720)
   };
+  
+  std::vector<dx::app::AssetManager::FontDesc> fontDescs;
+  fontDescs.reserve(5);
+  fontDescs.emplace_back(U"Title", 120, U"example/font/AnnyantRoman/AnnyantRoman.ttf");
+  fontDescs.emplace_back(U"Menu" ,  30, s3d::Typeface::Regular);
+  fontDescs.emplace_back(U"Score",  36, s3d::Typeface::Bold);
+  
   return dx::app::ExecutiveManager::Desc{
-    .application = appDesc
+    .application = appDesc,
+    .font = fontDescs
   };
 }
 
