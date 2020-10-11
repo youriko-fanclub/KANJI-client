be225f98723a2e02490e7f515f6b049047cb3c3e
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-10-12 02:31:27 +0900

    TextureAsset、TitleにTexture入稿

--- Game/KANJI/src/TitleScene.cpp
+++ Game/KANJI/src/TitleScene.cpp
@@ -7,19 +7,13 @@ namespace seq {
 TitleScene::TitleScene(const InitData& init) :
 IScene(init),
 m_start(
-  Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60),
+  Rect(Arg::center = Scene::Center().movedBy(65, 170), 300, 60),
   DrawableText(FontAsset(U"Menu"), U"はじめる"),
   Transition(0.4s, 0.2s)),
 m_exit(
-  Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60),
+  Rect(Arg::center = Scene::Center().movedBy(65, 250), 300, 60),
   DrawableText(FontAsset(U"Menu"), U"おわる"),
   Transition(0.4s, 0.2s)) {
-  {
-    const String titleText = U"漢字発見! モジバトル";
-    const Vec2 center(Scene::Center().x, 120);
-    m_title.first = FontAsset(U"Title")(titleText);
-    m_title.second = FontAsset(U"Title")(titleText);
-  }
   
   m_start.setCallback([this](){ changeScene(State::Game); });
   m_exit.setCallback([](){ System::Exit(); });
@@ -31,6 +25,9 @@ void TitleScene::update() {
 }
 
 void TitleScene::draw() const {
+  Scene::Rect()(TextureAsset(U"Title::Bg")).draw();
+  TextureAsset(U"Title::Logo").drawAt(Scene::Center().movedBy(55, -90));
+
   const Vec2 center(Scene::Center().x, 120);
   m_title.first.drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
   m_title.second.drawAt(center);
@@ -40,9 +37,6 @@ void TitleScene::draw() const {
   
   Rect(0, 500, Scene::Width(), Scene::Height() - 500)
   .draw(Arg::top = ColorF(0.0, 0.0), Arg::bottom = ColorF(0.0, 0.5));
-  
-  const int32 highScore = getData().highScore;
-  FontAsset(U"Score")(U"High score: {}"_fmt(highScore)).drawAt(Vec2(620, 550));
 }
 
 
