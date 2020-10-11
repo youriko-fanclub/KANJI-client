05ce78d6d41c1b9596e95ef73e60cd0e9e4bd5c8
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-10-11 23:36:39 +0900

    struct Button

--- Game/KANJI/src/TitleScene.cpp
+++ Game/KANJI/src/TitleScene.cpp
@@ -4,52 +4,45 @@ namespace kanji {
 namespace seq {
 
 
-TitleScene::TitleScene(const InitData& init)
-	: IScene(init)
-{
-
-}
-
-void TitleScene::update()
-{
-	m_startTransition.update(m_startButton.mouseOver());
-	m_exitTransition.update(m_exitButton.mouseOver());
-
-	if (m_startButton.mouseOver() || m_exitButton.mouseOver())
-	{
-		Cursor::RequestStyle(CursorStyle::Hand);
-	}
-
-	if (m_startButton.leftClicked())
-	{
-		changeScene(State::Game);
-	}
-
-	if (m_exitButton.leftClicked())
-	{
-		System::Exit();
-	}
-}
-
-void TitleScene::draw() const
-{
-	// const String titleText = U"ブロックくずし";
+TitleScene::TitleScene(const InitData& init) :
+IScene(init),
+m_start(
+  Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60),
+  DrawableText(FontAsset(U"Menu"), U"はじめる"),
+  Transition(0.4s, 0.2s)),
+m_exit(
+  Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60),
+  DrawableText(FontAsset(U"Menu"), U"おわる"),
+  Transition(0.4s, 0.2s)) {
+  {
     const String titleText = U"クレイジーサイコレズ";
-	const Vec2 center(Scene::Center().x, 120);
-	FontAsset(U"Title")(titleText).drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
-	FontAsset(U"Title")(titleText).drawAt(center);
+    const Vec2 center(Scene::Center().x, 120);
+    m_title.first = FontAsset(U"Title")(titleText);
+    m_title.second = FontAsset(U"Title")(titleText);
+  }
+  
+  m_start.setCallback([this](){ changeScene(State::Game); });
+  m_exit.setCallback([](){ System::Exit(); });
+}
 
-	m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
-	m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
+void TitleScene::update() {
+  m_start.update();
+  m_exit.update();
+}
 
-	FontAsset(U"Menu")(U"はじめる").drawAt(m_startButton.center(), ColorF(0.25));
-	FontAsset(U"Menu")(U"おわる").drawAt(m_exitButton.center(), ColorF(0.25));
-
-	Rect(0, 500, Scene::Width(), Scene::Height() - 500)
-		.draw(Arg::top = ColorF(0.0, 0.0), Arg::bottom = ColorF(0.0, 0.5));
-
-	const int32 highScore = getData().highScore;
-	FontAsset(U"Score")(U"High score: {}"_fmt(highScore)).drawAt(Vec2(620, 550));
+void TitleScene::draw() const {
+  const Vec2 center(Scene::Center().x, 120);
+  m_title.first.drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
+  m_title.second.drawAt(center);
+  
+  m_start.draw();
+  m_exit.draw();
+  
+  Rect(0, 500, Scene::Width(), Scene::Height() - 500)
+  .draw(Arg::top = ColorF(0.0, 0.0), Arg::bottom = ColorF(0.0, 0.5));
+  
+  const int32 highScore = getData().highScore;
+  FontAsset(U"Score")(U"High score: {}"_fmt(highScore)).drawAt(Vec2(620, 550));
 }
 
 
--- Game/KANJI/src/TitleScene.hpp
+++ Game/KANJI/src/TitleScene.hpp
@@ -8,21 +8,70 @@ namespace seq {
 
 // タイトルシーン
 class TitleScene : public KanjiScene {
+public: // static_const/enum
+  class Button {
+  public: // static_const/enum
+  public: // static
+  public: // public getter
+    const s3d::Rect& rect() const { return m_rect; }
+  public: // public setter
+    void setCallback(const std::function<void()>& callback) {
+      m_pressedCallback = callback;
+    }
+    bool update() {
+      m_transition.update(m_rect.mouseOver());
+      if (rect().mouseOver()) {
+        Cursor::RequestStyle(CursorStyle::Hand);
+      }
+      if (rect().leftClicked()) {
+        m_pressedCallback();
+      }
+      return true;
+    }
+    void draw() const {
+      m_rect.draw(ColorF(1.0, m_transition.value())).drawFrame(2);
+      m_label.drawAt(m_rect.center(), ColorF(0.25));
+    }
+  private: // field
+    s3d::Rect m_rect;
+    s3d::DrawableText m_label;
+    Transition m_transition;
+    std::function<void()> m_pressedCallback = [](){};
+  private: // private function
+  public: // ctor/dtor
+    Button(
+      const s3d::Rect& rect,
+      const s3d::DrawableText& label,
+      const Transition& transition,
+      const std::function<void()>& pressedCallback) :
+      m_rect(rect), m_label(label), m_transition(transition), m_pressedCallback(pressedCallback) {}
+    Button(const s3d::Rect& rect, const s3d::DrawableText& label, const Transition& transition) :
+      m_rect(rect), m_label(label), m_transition(transition) {}
+  };
+public: // static
+public: // public function
+private: // field
+  std::pair<DrawableText, DrawableText> m_title;
+  Button m_start;
+  Button m_exit;
+private: // private function
+public: // ctor/dtor
+
 private:
-
-	Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
-	Transition m_startTransition = Transition(0.4s, 0.2s);
-
-	Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
-	Transition m_exitTransition = Transition(0.4s, 0.2s);
+  
+  Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
+  Transition m_startTransition = Transition(0.4s, 0.2s);
+  
+  Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
+  Transition m_exitTransition = Transition(0.4s, 0.2s);
 
 public:
-
-	TitleScene(const InitData& init);
-
-	void update() override;
-
-	void draw() const override;
+  
+  TitleScene(const InitData& init);
+  
+  void update() override;
+  
+  void draw() const override;
 };
 
 
