315161bdd267315c2c3bc43372300a87c8ae597a
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-11-02 23:42:18 +0900

    tomlをphysicsテーブルで包んだので変更

--- Game/KANJI/src/parameter/CharaPhysicsParameters.cpp
+++ Game/KANJI/src/parameter/CharaPhysicsParameters.cpp
@@ -10,12 +10,12 @@ namespace param {
 // protected function -------------------------------
 void CharaPhysics::loadImpl(const s3d::TOMLReader& toml) {
     // 要素のパスで値を取得
-    force_horizontal = toml[U"chara.force.horizontal"].get<double>();
-    force_jump       = toml[U"chara.force.jump"].get<double>();
-    chara_friction   = toml[U"chara.friction"].get<double>();
-    air_resistance   = toml[U"chara.air_resistance"].get<double>();
-    floor_friction   = toml[U"floor.friction"].get<double>();
-    wall_friction    = toml[U"wall.friction"].get<double>();
+    force_horizontal = toml[U"physics.chara.force.horizontal"].get<double>();
+    force_jump       = toml[U"physics.chara.force.jump"].get<double>();
+    chara_friction   = toml[U"physics.chara.friction"].get<double>();
+    air_resistance   = toml[U"physics.chara.air_resistance"].get<double>();
+    floor_friction   = toml[U"physics.floor.friction"].get<double>();
+    wall_friction    = toml[U"physics.wall.friction"].get<double>();
 }
 // private function ------------------------------
 // ctor/dtor -------------------------------------
