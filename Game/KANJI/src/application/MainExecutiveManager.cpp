cbb2fe30cd805c8fcf1f9ab8fb1ade92ce06f5b8
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-11-03 11:02:52 +0900

    インデント修正

--- Game/KANJI/src/application/MainExecutiveManager.cpp
+++ Game/KANJI/src/application/MainExecutiveManager.cpp
@@ -4,31 +4,31 @@ namespace kanji {
 namespace app {
 
 dx::app::ExecutiveManager::Desc createExecutiveDesc() {
-  const dx::app::ApplicationManager::Desc appDesc = {
-    .isCloseByEscapeKey = true,
-    .windowTitle = U"漢字発見! 文字バトル",
-    // .windowSize = s3d::Size(1280, 720)
-    .windowSize = s3d::Size(1024, 768)
-  };
-  
-  std::vector<dx::app::AssetManager::FontDesc> fontDescs;
-  fontDescs.reserve(5);
-  fontDescs.emplace_back(U"System", 20, s3d::Typeface::Bold);
-  // fontDescs.emplace_back(U"Title", 120, U"example/font/AnnyantRoman/AnnyantRoman.ttf");
-  fontDescs.emplace_back(U"Title", 120, U"KouzanMouhituFontOTF.otf");
-  fontDescs.emplace_back(U"Menu" ,  40, U"azuki.ttf");
-  fontDescs.emplace_back(U"Score",  36, s3d::Typeface::Bold);
-
-  std::vector<dx::app::AssetManager::TextureDesc> textureDescs;
-  textureDescs.reserve(5);
-  textureDescs.emplace_back(U"Title::Logo", U"Title/Logo");
-  textureDescs.emplace_back(U"Title::Bg", U"Title/Bg");
-
-  return dx::app::ExecutiveManager::Desc{
-    .application = appDesc,
-    .font = fontDescs,
-    .texture = textureDescs
-  };
+    const dx::app::ApplicationManager::Desc appDesc = {
+        .isCloseByEscapeKey = true,
+        .windowTitle = U"漢字発見! 文字バトル",
+        // .windowSize = s3d::Size(1280, 720)
+        .windowSize = s3d::Size(1024, 768)
+    };
+    
+    std::vector<dx::app::AssetManager::FontDesc> fontDescs;
+    fontDescs.reserve(5);
+    fontDescs.emplace_back(U"System", 20, s3d::Typeface::Bold);
+    // fontDescs.emplace_back(U"Title", 120, U"example/font/AnnyantRoman/AnnyantRoman.ttf");
+    fontDescs.emplace_back(U"Title", 120, U"KouzanMouhituFontOTF.otf");
+    fontDescs.emplace_back(U"Menu" ,  40, U"azuki.ttf");
+    fontDescs.emplace_back(U"Score",  36, s3d::Typeface::Bold);
+    
+    std::vector<dx::app::AssetManager::TextureDesc> textureDescs;
+    textureDescs.reserve(5);
+    textureDescs.emplace_back(U"Title::Logo", U"Title/Logo");
+    textureDescs.emplace_back(U"Title::Bg", U"Title/Bg");
+    
+    return dx::app::ExecutiveManager::Desc{
+        .application = appDesc,
+        .font = fontDescs,
+        .texture = textureDescs
+    };
 }
 
 }
