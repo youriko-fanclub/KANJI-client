80e36582741a875b55fa472c9306a13b46130c59
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-11-22 04:33:40 +0900

    実装をcppに移動

--- Game/KANJI/src/battle/physics/PhysicalMove.cpp
+++ Game/KANJI/src/battle/physics/PhysicalMove.cpp
@@ -3,5 +3,115 @@
 namespace kanji {
 namespace battle {
 
+/* ---------- MomentaryMove ---------- */
+
+// static ----------------------------------------
+MomentaryMove MomentaryMove::divideLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin) {
+    const float s = t / (to.time - from.time);
+    // TOdO: とりあえず全部線形補間してるけど変えたほうが良さそう
+    return MomentaryMove(
+        t,
+        static_cast<int>(s * to.damage + (1-s) * from.damage),
+        s3d::Circular( // TOdO: theta を isRight によって切り替える
+            s * to.shoot_force.r + (1-s) * from.shoot_force.r,
+            s * to.shoot_force.theta + (1-s) * from.shoot_force.theta),
+        divideHitboxLinear(t, from, to, is_right, origin)
+    );
+}
+s3d::RectF MomentaryMove::divideHitboxLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin) {
+    const float s = (t - from.time) / (to.time - from.time);
+    return s3d::RectF(s3d::Arg::center(
+        (s * to.hitbox.center().x + (1.f-s) * from.hitbox.center().x) * (is_right ? 1.f : -1.f) + origin.x,
+        s * to.hitbox.center().y + (1.f-s) * from.hitbox.center().y + origin.y),
+        s * to.hitbox.w + (1.f-s) * from.hitbox.w,
+        s * to.hitbox.h + (1.f-s) * from.hitbox.h);
+}
+// public function -------------------------------
+// private function ------------------------------
+// ctor/dtor -------------------------------------
+MomentaryMove::MomentaryMove(float time, int damage, const s3d::Circular& shoot_force, const s3d::RectF& hitbox) :
+time(time), damage(damage), shoot_force(shoot_force), hitbox(hitbox) {}
+
+
+/* ---------- Trajectory ---------- */
+
+// static ----------------------------------------
+// public function -------------------------------
+MomentaryMove Trajectory::momentary(float time, bool is_right, const s3d::Vec2& origin) const {
+    int index = -1;
+    for (int i = 0; i < m_moments.size(); ++i) {
+        if (time <= m_moments.at(i).time) {
+            index = i;
+        }
+    }
+    // if (index < 0) { return; }
+    return MomentaryMove::divideLinear(time, m_moments.at(index - 1), m_moments.at(index), is_right, origin);
+}
+
+s3d::RectF Trajectory::momentaryHitbox(float time, bool is_right, const s3d::Vec2& origin) const {
+    int index = -1;
+    for (int i = 0; i < m_moments.size(); ++i) {
+        if (time <= m_moments.at(i).time) {
+            index = i;
+            break;
+        }
+    }
+    // if (index < 0) { return; }
+    return MomentaryMove::divideHitboxLinear(time, m_moments.at(index - 1), m_moments.at(index), is_right, origin);
+}
+
+// private function ------------------------------
+// ctor/dtor -------------------------------------
+
+
+/* ---------- PhysicalMove ---------- */
+
+// static ----------------------------------------
+// public function -------------------------------
+MomentaryMove PhysicalMove::currentMoment() const {
+    return m_md->trajectory->momentary(m_timer, m_is_right, m_ownerChara->position());
+}
+s3d::RectF PhysicalMove::currentHitBox() const {
+    return m_md->trajectory->momentaryHitbox(m_timer, m_is_right, m_ownerChara->position());
+}
+
+bool PhysicalMove::update(float dt) {
+    m_timer += dt;
+    return m_timer > m_md->trajectory->totalTime();
+}
+// private function ------------------------------
+// ctor/dtor -------------------------------------
+PhysicalMove::PhysicalMove(dx::di::PlayerId owner, const std::shared_ptr<battle::PhysicalCharacter>& ownerChara, md::MoveId moveId) :
+    m_timer(0),
+    m_owner(owner),
+    m_is_right(ownerChara->isRight()),
+    m_ownerChara(ownerChara),
+    m_md(md::MasterRepository::instance()->get(moveId)) {}
+
+
+/* ---------- PhysicalMoveManager ---------- */
+
+// static ----------------------------------------
+// public function -------------------------------
+const std::shared_ptr<PhysicalMove>& PhysicalMoveManager::createMove(dx::di::PlayerId owner, const std::shared_ptr<PhysicalCharacter>& ownerChara, md::MoveId moveId) {
+    m_moves.push_back(std::make_shared<PhysicalMove>(owner, ownerChara, moveId));
+    return m_moves.back();
+}
+void PhysicalMoveManager::update(float dt) {
+    for (auto it = m_moves.begin(); it != m_moves.end();) {
+        if ((*it)->update(dt)) {
+            it = m_moves.erase(it);
+        }
+        else { ++it; }
+    }
+}
+
+const std::vector<std::shared_ptr<PhysicalMove>>& PhysicalMoveManager::moves() const {
+    return m_moves;
+}
+// private function ------------------------------
+// ctor/dtor -------------------------------------
+
+
 }
 }
--- Game/KANJI/src/battle/physics/PhysicalMove.hpp
+++ Game/KANJI/src/battle/physics/PhysicalMove.hpp
@@ -5,7 +5,6 @@
 #include "PlayerId.hpp"
 #include "Move.hpp"
 #include "PhysicalCharacter.hpp"
-#include "Log.hpp"
 
 namespace kanji {
 namespace battle {
@@ -17,29 +16,10 @@ public: // field
     const s3d::Circular shoot_force;
     const s3d::RectF hitbox;
 public: // static
-    static MomentaryMove divideLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin) {
-        const float s = t / (to.time - from.time);
-        // TOdO: とりあえず全部線形補間してるけど変えたほうが良さそう
-        return MomentaryMove(
-            t,
-            static_cast<int>(s * to.damage + (1-s) * from.damage),
-            s3d::Circular( // TOdO: theta を isRight によって切り替える
-                s * to.shoot_force.r + (1-s) * from.shoot_force.r,
-                s * to.shoot_force.theta + (1-s) * from.shoot_force.theta),
-            divideHitboxLinear(t, from, to, is_right, origin)
-        );
-    }
-    static s3d::RectF divideHitboxLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin) {
-        const float s = (t - from.time) / (to.time - from.time);
-        return s3d::RectF(s3d::Arg::center(
-            (s * to.hitbox.center().x + (1.f-s) * from.hitbox.center().x) * (is_right ? 1.f : -1.f) + origin.x,
-            s * to.hitbox.center().y + (1.f-s) * from.hitbox.center().y + origin.y),
-            s * to.hitbox.w + (1.f-s) * from.hitbox.w,
-            s * to.hitbox.h + (1.f-s) * from.hitbox.h);
-    }
+    static MomentaryMove divideLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin);
+    static s3d::RectF divideHitboxLinear(float t, const MomentaryMove& from, const MomentaryMove& to, bool is_right, const s3d::Vec2& origin);
 public: // ctor/dtor
-    MomentaryMove(float time, int damage, const s3d::Circular& shoot_force, const s3d::RectF& hitbox) :
-    time(time), damage(damage), shoot_force(shoot_force), hitbox(hitbox) {}
+    MomentaryMove(float time, int damage, const s3d::Circular& shoot_force, const s3d::RectF& hitbox);
 };
 
 class Trajectory {
@@ -47,27 +27,8 @@ public: // static_const/enum
 public: // static
 public: // public function
     float totalTime() const { return m_moments.back().time; }
-    MomentaryMove momentary(float time, bool is_right, const s3d::Vec2& origin) const {
-        int index = -1;
-        for (int i = 0; i < m_moments.size(); ++i) {
-            if (time <= m_moments.at(i).time) {
-                index = i;
-            }
-        }
-        // if (index < 0) { return; }
-        return MomentaryMove::divideLinear(time, m_moments.at(index - 1), m_moments.at(index), is_right, origin);
-    }
-    s3d::RectF momentaryHitbox(float time, bool is_right, const s3d::Vec2& origin) const {
-        int index = -1;
-        for (int i = 0; i < m_moments.size(); ++i) {
-            if (time <= m_moments.at(i).time) {
-                index = i;
-                break;
-            }
-        }
-        // if (index < 0) { return; }
-        return MomentaryMove::divideHitboxLinear(time, m_moments.at(index - 1), m_moments.at(index), is_right, origin);
-    }
+    MomentaryMove momentary(float time, bool is_right, const s3d::Vec2& origin) const;
+    s3d::RectF momentaryHitbox(float time, bool is_right, const s3d::Vec2& origin) const;
 private: // field
     std::vector<MomentaryMove> m_moments;
 private: // private function
@@ -83,18 +44,11 @@ public: // static_const/enum
 public: // static
 public: // public function
     dx::di::PlayerId owner() const { return m_owner; }
-    MomentaryMove currentMoment() const {
-        return m_md->trajectory->momentary(m_timer, m_is_right, m_ownerChara->position());
-    }
-    s3d::RectF currentHitBox() const {
-        return m_md->trajectory->momentaryHitbox(m_timer, m_is_right, m_ownerChara->position());
-    }
+    MomentaryMove currentMoment() const;
+    s3d::RectF currentHitBox() const;
     
     // 終了したらtrueを返す
-    bool update(float dt) {
-        m_timer += dt;
-        return m_timer > m_md->trajectory->totalTime();
-    }
+    bool update(float dt);
 private: // field
     float m_timer;
     dx::di::PlayerId m_owner;
@@ -103,12 +57,7 @@ private: // field
     const std::shared_ptr<Move>& m_md;
 private: // private function
 public: // ctor/dtor
-    PhysicalMove(dx::di::PlayerId owner, const std::shared_ptr<battle::PhysicalCharacter>& ownerChara, md::MoveId moveId) :
-        m_timer(0),
-        m_owner(owner),
-        m_is_right(ownerChara->isRight()),
-        m_ownerChara(ownerChara),
-        m_md(md::MasterRepository::instance()->get(moveId)) {}
+    PhysicalMove(dx::di::PlayerId owner, const std::shared_ptr<battle::PhysicalCharacter>& ownerChara, md::MoveId moveId);
 };
 
 
@@ -116,22 +65,10 @@ class PhysicalMoveManager {
 public: // static_const/enum
 public: // static
 public: // public function
-    const std::shared_ptr<PhysicalMove>& createMove(dx::di::PlayerId owner, const std::shared_ptr<PhysicalCharacter>& ownerChara, md::MoveId moveId) {
-        m_moves.push_back(std::make_shared<PhysicalMove>(owner, ownerChara, moveId));
-        return m_moves.back();
-    }
-    void update(float dt) {
-        for (auto it = m_moves.begin(); it != m_moves.end();) {
-            if ((*it)->update(dt)) {
-                it = m_moves.erase(it);
-            }
-            else { ++it; }
-        }
-    }
+    const std::shared_ptr<PhysicalMove>& createMove(dx::di::PlayerId owner, const std::shared_ptr<PhysicalCharacter>& ownerChara, md::MoveId moveId);
+    void update(float dt);
     
-    const std::vector<std::shared_ptr<PhysicalMove>>& moves() const {
-        return m_moves;
-    }
+    const std::vector<std::shared_ptr<PhysicalMove>>& moves() const;
 private: // field
     std::vector<std::shared_ptr<PhysicalMove>> m_moves;
 private: // private function
