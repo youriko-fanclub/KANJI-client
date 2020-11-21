2aae860ef973fef98065292cf7cc3668bef41b32
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2020-11-22 01:00:22 +0900

    テスト用の技(拳)の軌道を修正

--- Game/KANJI/src/masterdata/Move.cpp
+++ Game/KANJI/src/masterdata/Move.cpp
@@ -14,20 +14,22 @@ MasterRepository::MasterRepository() {
     // TOdO: デバッグ用
     std::vector<battle::MomentaryMove> moments;
     float t = 0.f;
+    moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(0, 0), s3d::Size(60, 60)));
+    t += 0.1f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(20, 0), s3d::Size(60, 60)));
-    t += 2.f;
+    t += 0.2f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(40, 0), s3d::Size(60, 60)));
-    t += 2.f;
+    t += 0.2f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(60, 0), s3d::Size(60, 60)));
-    t += 1.f;
+    t += 0.1f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(60, 0), s3d::Size(60, 60)));
-    t += 7.f;
+    t += 0.7f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(40, 0), s3d::Size(60, 60)));
-    t += 8.f;
+    t += 0.8f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(30, 0), s3d::Size(60, 60)));
-    t += 3.f;
+    t += 0.3f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center(10, 0), s3d::Size(60, 60)));
-    t += 2.f;
+    t += 0.2f;
     moments.emplace_back(t, 50.f, s3d::Circular(-35, 500), s3d::RectF(s3d::Arg::center( 0, 0), s3d::Size(60, 60)));
     const MoveId id = 0;
     m_masterdata.insert(std::make_pair(id,
