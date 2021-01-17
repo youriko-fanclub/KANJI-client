2e45f1bcaf51d743d7c0f9f485bc1947d5fdcf04
Yuta Ishimatsu <yuta.ishimatsu@gree.net>
2021-01-17 23:03:55 +0900

    Update

--- Game/KANJI/src/characterselection/CharacterSelectionManager.cpp
+++ Game/KANJI/src/characterselection/CharacterSelectionManager.cpp
@@ -1,17 +1,30 @@
 #include "CharacterSelectionManager.hpp"
 
+#include <Siv3D/Keyboard.hpp>
+
 namespace kanji {
 namespace cs {
 
 CharacterSelectionManager::CharacterSelectionManager() :
-    m_panel_grid{},
+    m_panel_grid{ { 50, 50 },  { 100, 100 } , 5 , 1 },
     m_cursor_manager{}
-{}
+{
+    // ファイルから読み込みたいよね
+    m_panel_grid.Add(U"安");
+    m_panel_grid.Add(U"達");
+    m_panel_grid.Add(U"曜");
+    m_panel_grid.Add(U"梨");
+    m_panel_grid.Add(U"子");
+}
 
 void CharacterSelectionManager::update()
 {
     m_panel_grid.update();
     m_cursor_manager.update();
+    
+    if (s3d::KeyA.up()) {
+        m_panel_grid.Add(U"王");
+    }
 }
 
 void CharacterSelectionManager::draw() const
